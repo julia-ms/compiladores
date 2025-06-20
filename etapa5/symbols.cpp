a01f8af206a2b8248ebf47c9a455447c7840178e
julia <mombachdasilva@gmail.com>
2025-06-19 21:15:56 -0300

    etapa5

---
+++ etapa5/symbols.cpp
@@ -0,0 +1,77 @@
+// Júlia Mombach da Silva
+
+#include "symbols.hpp"
+#include <string>
+#include <map>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+map<string, SYMBOL*> SymbolTable;
+
+map<int, string> TokenNamesMap = {
+    {SYMBOL_LIT_INT, "SYMBOL_LIT_INT"},
+    {SYMBOL_LIT_REAL, "SYMBOL_LIT_REAL"},
+    {SYMBOL_LIT_CHAR, "SYMBOL_LIT_CHAR"},
+    {SYMBOL_LIT_STRING, "SYMBOL_LIT_STRING"},
+    {SYMBOL_TK_IDENTIFIER, "SYMBOL_TK_IDENTIFIER"},
+    {SYMBOL_TK_IDENTIFIER_VAR, "SYMBOL_TK_IDENTIFIER_VAR"},
+    {SYMBOL_TK_IDENTIFIER_VECTOR, "SYMBOL_TK_IDENTIFIER_VECTOR"},
+    {SYMBOL_TK_IDENTIFIER_FUNCTION, "SYMBOL_TK_IDENTIFIER_FUNCTION"},
+    {SYMBOL_VECTOR_SIZE, "SYMBOL_VECTOR_SIZE"}
+};
+
+map<int, string> SymbolsDataTypeNameMap = {
+    {DATATYPE_UNDEFINED, "DATATYPE_UNDEFINED"},
+    {DATATYPE_INT, "DATATYPE_INT"},
+    {DATATYPE_REAL, "DATATYPE_REAL"},
+    {DATATYPE_CHAR, "DATATYPE_CHAR"},
+    {DATATYPE_STRING, "DATATYPE_STRING"},
+    {DATATYPE_BYTE, "DATATYPE_BYTE"},
+    {DATATYPE_BOOL, "DATATYPE_BOOL"},
+    {DATATYPE_INVALID, "DATATYPE_INVALID"}
+};
+
+SYMBOL *symbolInsert(int type, char *text){
+    if(SymbolTable.find(string(text)) != SymbolTable.end()){
+        return SymbolTable.find(string(text))->second;
+    }
+    SYMBOL *newSymbol = new SYMBOL(type, string(text));
+    SymbolTable[string(text)] = newSymbol;
+    return newSymbol;
+
+}
+
+void printSymbolsTable(){
+    for(auto s: SymbolTable){
+        string typeName = (TokenNamesMap.find(s.second->type) != TokenNamesMap.end()) 
+                            ? TokenNamesMap[s.second->type] 
+                            : to_string(s.second->type);
+        string dataTypeName = (SymbolsDataTypeNameMap.find(s.second->dataType) != SymbolsDataTypeNameMap.end())
+                            ? SymbolsDataTypeNameMap[s.second->dataType]
+                            : to_string(s.second->dataType);
+        printf("Symbol[%s, %s], datatype: %s\n", typeName.c_str(), s.second->text.c_str(), dataTypeName.c_str());
+    }
+}
+
+SYMBOL* makeTemp(void){
+    static int serialNumber = 0;
+    static char buffer[32] = "";
+    snprintf(buffer,32, "temp%d", serialNumber++);
+    return symbolInsert(SYMBOL_TK_IDENTIFIER, buffer);
+}
+
+SYMBOL* makeLabel(void){
+    static int serialNumber = 0;
+    static char buffer[32] = "";
+    snprintf(buffer,32, "label%d", serialNumber++);
+    return symbolInsert(SYMBOL_TK_IDENTIFIER, buffer);
+}
+
+SYMBOL* makeConst(int value) {
+    char buffer[64];
+    sprintf(buffer, "%d", value);
+    return symbolInsert(SYMBOL_LIT_INT, buffer);
+}
---
+++ etapa5/symbols.hpp
@@ -0,0 +1,55 @@
+// Júlia Mombach da Silva
+
+#ifndef SYMBOLS_HEADER
+#define SYMBOLS_HEADER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+struct SYMBOL {
+    int type;
+    int dataType;
+    string text;
+    public:
+        SYMBOL(int t, string s): type(t), text(s) {};
+};
+
+enum TokenNames{
+    SYMBOL_LIT_INT = 1,
+    SYMBOL_LIT_REAL = 2,
+    SYMBOL_LIT_STRING = 3,
+    SYMBOL_LIT_CHAR = 4,
+    SYMBOL_LIT_BYTE = 5,
+    SYMBOL_TK_IDENTIFIER = 7,
+    SYMBOL_TK_IDENTIFIER_VAR = 8,
+    SYMBOL_TK_IDENTIFIER_VECTOR = 9,
+    SYMBOL_TK_IDENTIFIER_FUNCTION = 10,
+    SYMBOL_VECTOR_SIZE=11
+};
+
+enum SymbolsDataTypeName{
+    DATATYPE_UNDEFINED  = 0,
+    DATATYPE_INT = 1,
+    DATATYPE_REAL = 2,
+    DATATYPE_STRING = 3,
+    DATATYPE_CHAR = 4,
+    DATATYPE_BYTE = 5,
+    DATATYPE_BOOL = 6,
+    DATATYPE_VECTOR = 7,
+    DATATYPE_INVALID = 8
+};
+
+SYMBOL *symbolInsert(int type, char *text);
+void printSymbolsTable();
+
+// TACS
+SYMBOL* makeTemp();
+SYMBOL* makeLabel();
+SYMBOL* makeConst(int value);
+
+
+
+#endif
